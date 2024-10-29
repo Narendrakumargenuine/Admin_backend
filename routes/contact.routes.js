8e4ae8ce51d335323aef324e36e0d3f1005605d8
mdShahbazUddin1 <jackayron5@gmail.com>
2024-10-29 17:24:10 +0530

    done

---
+++ routes/contact.routes.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const multer = require("multer");
+const {
+  saveContact,
+  getAllContacts,
+  updateStatusById,
+  getPendingContacts,
+  getRejectedContacts,
+} = require("../controller/ContactUs.controller");
+const contactRouter = express.Router();
+
+// Configure multer to handle file uploads
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+// Define the route to save contact with image uploads
+contactRouter.post("/contacts", upload.array("images", 10), saveContact); // Allows up to 10 images
+contactRouter.get("/get", getAllContacts); // Allows up to 10 images
+contactRouter.put("/contact/:id/status", updateStatusById);
+contactRouter.get("/contacts/pending", getPendingContacts);
+contactRouter.get("/contacts/rejected", getRejectedContacts);
+
+module.exports = contactRouter;
